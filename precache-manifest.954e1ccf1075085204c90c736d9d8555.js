90db3bffd36aab051096b939fdd4395e360b8e6e
Marvin Milanzi <dsmmarvini6.0@gmail.com>
2020-09-14 06:58:05 +0200

    Updates

---
+++ precache-manifest.954e1ccf1075085204c90c736d9d8555.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68e06928d0fa42853b7d7db2ba809b30",
+    "url": "/memefy/index.html"
+  },
+  {
+    "revision": "b3326fd88d3679cbe97e",
+    "url": "/memefy/static/css/main.30d6b5a6.chunk.css"
+  },
+  {
+    "revision": "c43273d8225311dc5ded",
+    "url": "/memefy/static/js/2.0c6ae9ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memefy/static/js/2.0c6ae9ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3326fd88d3679cbe97e",
+    "url": "/memefy/static/js/main.b80e4ef9.chunk.js"
+  },
+  {
+    "revision": "f4205333a0932376f25c",
+    "url": "/memefy/static/js/runtime-main.2011c27a.js"
+  },
+  {
+    "revision": "42fd59f36c2117f842d280d6fb97a93c",
+    "url": "/memefy/static/media/logo2.42fd59f3.png"
+  },
+  {
+    "revision": "89245447530b05a2f1b882bc323921f4",
+    "url": "/memefy/static/media/memeimg.89245447.jpg"
+  }
+]);
